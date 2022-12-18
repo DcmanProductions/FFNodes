88f5d50f58bd32473cabfaaf274da189f4d91b8a
Drew Chase <dcmanproductions@gmail.com>
2022-12-17 23:32:03 -0500

    Added static class to handle Locations

---
+++ FFNodes.Core/Utilities/Locations.cs
@@ -0,0 +1,10 @@
+﻿// LFInteractive LLC. - All Rights Reserved
+namespace FFNodes.Core.Utilities;
+
+public static class Locations
+{
+    public static string Data => Directory.CreateDirectory(Path.Combine(Root, "data")).FullName;
+    public static string NodeData => Directory.CreateDirectory(Path.Combine(Data, "nodes")).FullName;
+    public static string Root => Directory.CreateDirectory(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), ApplicationData.CompanyName, ApplicationData.ApplicationName)).FullName;
+    public static string Temp => Directory.CreateDirectory(Path.Combine(Root, "tmp")).FullName;
+}
