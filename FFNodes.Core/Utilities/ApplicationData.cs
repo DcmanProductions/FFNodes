55fe50673d2f00cad86f1579b0d9d4e570417cc4
Drew Chase <dcmanproductions@gmail.com>
2022-12-17 23:31:24 -0500

    Added library to handle ApplicationData

---
+++ FFNodes.Core/Utilities/ApplicationData.cs
@@ -0,0 +1,10 @@
+﻿// LFInteractive LLC. - All Rights Reserved
+namespace FFNodes.Core.Utilities;
+
+public static class ApplicationData
+{
+    public static string ApplicationName => "FFNodes";
+    public static string CompanyName => "LFInteractive";
+    public static int ServerAPIPort => 32534;
+    public static int ServerWebPort => 1034;
+}
