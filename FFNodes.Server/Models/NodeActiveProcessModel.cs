9e0e97813e5cc1f326d7afc222c0b7a185809c49
Drew Chase <dcmanproductions@gmail.com>
2022-12-17 23:32:41 -0500

    Added active process node

---
+++ FFNodes.Server/Models/NodeActiveProcessModel.cs
@@ -0,0 +1,26 @@
+﻿// LFInteractive LLC. - All Rights Reserved
+namespace FFNodes.Server.Models;
+
+[Serializable]
+public struct NodeActiveProcessModel
+{
+    /// <summary>
+    /// The current ffmpeg arguments
+    /// </summary>
+    public string FFmpegArgs { get; set; }
+
+    /// <summary>
+    /// The current ffmpeg completion percentage
+    /// </summary>
+    public double Percent { get; set; }
+
+    /// <summary>
+    /// The current size in bytes
+    /// </summary>
+    public long Size { get; set; }
+
+    /// <summary>
+    /// The current speed.
+    /// </summary>
+    public double Speed { get; set; }
+}
