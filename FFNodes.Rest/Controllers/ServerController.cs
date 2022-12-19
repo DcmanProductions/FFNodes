a1cccc144eb724246dc541bcf46c1f795c73b1a6
Drew Chase <dcmanproductions@gmail.com>
2022-12-19 11:05:31 -0500

    Added server controller and server status

---
+++ FFNodes.Rest/Controllers/ServerController.cs
@@ -0,0 +1,22 @@
+﻿// LFInteractive LLC. - All Rights Reserved
+using FFNodes.Core.Utilities;
+using FFNodes.Server.Collections;
+using FFNodes.Server.Utilities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FFNodes.Rest.Controllers;
+
+[Route("api")]
+[ApiController]
+public class ServerController : ControllerBase
+{
+    [HttpGet("")]
+    public IActionResult Status()
+    {
+        return new JsonResult(new
+        {
+            Uptime = Formatters.GetFormattedTime(ServerManager.Instance.GetUpTime()),
+            ActiveNodes = NodeCollection.Instance.ActiveNodes.Length
+        });
+    }
+}
