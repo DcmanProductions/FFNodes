77fda327aa8de29d3b508ec906a470b6965de113
Drew Chase <dcmanproductions@gmail.com>
2022-12-19 11:44:01 -0500

    Added class for managing the server

---
+++ FFNodes.Server/Utilities/ServerManager.cs
@@ -0,0 +1,16 @@
+﻿// LFInteractive LLC. - All Rights Reserved
+
+namespace FFNodes.Server.Utilities;
+
+public sealed class ServerManager
+{
+    public static ServerManager Instance = Instance ??= new();
+    private readonly long _start;
+
+    private ServerManager()
+    {
+        _start = DateTime.Now.Ticks;
+    }
+
+    public TimeSpan GetUpTime() => new(DateTime.Now.Ticks - _start);
+}
