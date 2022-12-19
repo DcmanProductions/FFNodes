057dcdc31d28143ad0aef48d2e4105b058e7c810
Drew Chase <dcmanproductions@gmail.com>
2022-12-19 11:04:20 -0500

    added utility class for all formatters

---
+++ FFNodes.Core/Utilities/Formatters.cs
@@ -0,0 +1,25 @@
+﻿// LFInteractive LLC. - All Rights Reserved
+using System.Text;
+
+namespace FFNodes.Core.Utilities;
+
+public static class Formatters
+{
+    public static string GetFormattedTime(TimeSpan span)
+    {
+        StringBuilder builder = new();
+
+        if (span.TotalDays >= 1)
+            builder.Append($" {span.Days} days ");
+        if (span.TotalHours >= 1)
+            builder.Append($" {span.Hours} hours ");
+        if (span.TotalMinutes >= 1)
+            builder.Append($" {span.Minutes} minutes ");
+        if (span.TotalSeconds >= 1)
+            builder.Append($" {span.Seconds} seconds ");
+        if (span.TotalMilliseconds >= 1)
+            builder.Append($" {span.Milliseconds} ms ");
+
+        return builder.ToString().Replace("  ", " ").Trim();
+    }
+}
