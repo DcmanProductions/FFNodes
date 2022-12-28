076a43dad89c952e457fb7fdbc3a7cdaadc699f3
Drew Chase <dcmanproductions@gmail.com>
2022-12-28 11:15:27 -0500

    Added ability to set, get, and delete scanned directories

---
+++ FFNodes.Rest/Controllers/SettingsController.cs
@@ -0,0 +1,52 @@
+﻿// LFInteractive LLC. - All Rights Reserved
+using FFNodes.Server.Collections;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FFNodes.Rest.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class SettingsController : ControllerBase
+{
+    [HttpDelete("directories")]
+    public IActionResult DeleteDirectories([FromForm] string[] directories)
+    {
+        ProcessFileCollection.Instance.RemoveDirectories(directories);
+        return Ok();
+    }
+
+    [HttpGet("directories")]
+    public IActionResult GetDirectories()
+    {
+        return new JsonResult(ProcessFileCollection.Instance.GetDirectories());
+    }
+
+    [HttpPost("directories")]
+    public IActionResult SetDirectories([FromForm] string[] directories)
+    {
+        List<string> existing_directories = new();
+        List<string> nonexisting_directories = new();
+        for (int i = 0; i < directories.Length; i++)
+        {
+            string directory = Path.GetFullPath(directories[i]);
+            if (Directory.Exists(directory))
+            {
+                existing_directories.Add(directory);
+            }
+            else
+            {
+                nonexisting_directories.Add(directory);
+            }
+        }
+        ProcessFileCollection.Instance.SetDirectories(existing_directories.ToArray());
+        if (nonexisting_directories.Any())
+        {
+            return Ok(new
+            {
+                message = "Some directories were invalid! They were not saved!",
+                nonexisting_directories
+            });
+        }
+        return Ok();
+    }
+}
